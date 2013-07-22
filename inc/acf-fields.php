4ef536914f0d0610f58991bec11116aa1903f804
Mike Pierce <mike@someoneandsons.net>
2013-07-22 11:41:26 +0200

    Fixing field definitions associated to columns template.

--- inc/acf-fields.php
+++ inc/acf-fields.php
@@ -1,29 +1,34 @@
 <?
+/**
+ *  Install Add-ons
+ *  
+ *  The following code will include all 4 premium Add-Ons in your theme.
+ *  Please do not attempt to include a file which does not exist. This will produce an error.
+ *  
+ *  All fields must be included during the 'acf/register_fields' action.
+ *  Other types of Add-ons (like the options page) can be included outside of this action.
+ *  
+ *  The following code assumes you have a folder 'add-ons' inside your theme.
+ *
+ *  IMPORTANT
+ *  Add-ons may be included in a premium theme as outlined in the terms and conditions.
+ *  However, they are NOT to be included in a premium / free plugin.
+ *  For more information, please read http://www.advancedcustomfields.com/terms-conditions/
+ */ 
 
-if(function_exists("register_field_group")) {
+/**
+ *  Register Field Groups
+ *
+ *  The register_field_group function accepts 1 array which holds the relevant data to register a field group
+ *  You may edit the array as you see fit. However, this may result in errors if the array is not compatible with ACF
+ */
+
+if(function_exists("register_field_group"))
+{
 	register_field_group(array (
 		'id' => 'acf_column-settings',
 		'title' => 'Column settings',
 		'fields' => array (
-			array (
-				'key' => 'field_51e65b42b92b9',
-				'label' => 'Column width',
-				'name' => 'column_width',
-				'type' => 'radio',
-				'instructions' => 'What fraction of the total width should this column occupy?',
-				'required' => 1,
-				'choices' => array (
-					12 => '1/2',
-					8 => '1/3',
-					6 => '1/4',
-					5 => '1/5',
-					4 => '1/6',
-				),
-				'other_choice' => 1,
-				'save_other_choice' => 0,
-				'default_value' => 12,
-				'layout' => 'horizontal',
-			),
 			array (
 				'key' => 'field_51e65c7e74688',
 				'label' => 'Column push',
@@ -51,7 +56,7 @@ if(function_exists("register_field_group")) {
 				array (
 					'param' => 'page_template',
 					'operator' => '==',
-					'value' => 'page-column copy.php',
+					'value' => 'page-column.php',
 					'order_no' => 0,
 					'group_no' => 0,
 				),
@@ -156,153 +161,6 @@ if(function_exists("register_field_group")) {
 				'default_value' => 'light',
 				'layout' => 'horizontal',
 			),
-			array (
-				'key' => 'field_51e66262c8d40',
-				'label' => 'Background type',
-				'name' => 'bg_type',
-				'type' => 'radio',
-				'instructions' => 'Color',
-				'choices' => array (
-					'color' => 'Color',
-					'image' => 'Image',
-					'gradient' => 'Gradient',
-				),
-				'other_choice' => 0,
-				'save_other_choice' => 0,
-				'default_value' => 'color',
-				'layout' => 'horizontal',
-			),
-			array (
-				'key' => 'field_51e662ae987cf',
-				'label' => 'Background color',
-				'name' => 'bg_color',
-				'type' => 'color_picker',
-				'instructions' => 'The background color you would like to display behind this section.',
-				'conditional_logic' => array (
-					'status' => 1,
-					'rules' => array (
-						array (
-							'field' => 'field_51e66262c8d40',
-							'operator' => '==',
-							'value' => 'color',
-						),
-					),
-					'allorany' => 'all',
-				),
-				'default_value' => '#ffffff',
-			),
-			array (
-				'key' => 'field_51e662286b4c1',
-				'label' => 'Background image',
-				'name' => 'bg_image',
-				'type' => 'image',
-				'instructions' => 'The background image you would like to display for this section.',
-				'conditional_logic' => array (
-					'status' => 1,
-					'rules' => array (
-						array (
-							'field' => 'field_51e66262c8d40',
-							'operator' => '==',
-							'value' => 'image',
-						),
-					),
-					'allorany' => 'all',
-				),
-				'save_format' => 'id',
-				'preview_size' => 'thumbnail',
-				'library' => 'uploadedTo',
-			),
-			array (
-				'key' => 'field_51e6706ee13a5',
-				'label' => 'Background image fixed',
-				'name' => 'bg_image_fixed',
-				'type' => 'radio',
-				'instructions' => 'Would you like the background image to be fixed to the window, or to move together with the section as the page scrolls?',
-				'conditional_logic' => array (
-					'status' => 1,
-					'rules' => array (
-						array (
-							'field' => 'field_51e66262c8d40',
-							'operator' => '==',
-							'value' => 'image',
-						),
-					),
-					'allorany' => 'all',
-				),
-				'choices' => array (
-					'' => 'Move with the section',
-					'fixed' => 'Fixed to the browser window',
-				),
-				'other_choice' => 0,
-				'save_other_choice' => 0,
-				'default_value' => '',
-				'layout' => 'horizontal',
-			),
-			array (
-				'key' => 'field_51e6632675b8a',
-				'label' => 'Background gradient type',
-				'name' => 'bg_gradient_type',
-				'type' => 'radio',
-				'instructions' => 'The type of gradient used for the background of this section.',
-				'conditional_logic' => array (
-					'status' => 1,
-					'rules' => array (
-						array (
-							'field' => 'field_51e66262c8d40',
-							'operator' => '==',
-							'value' => 'gradient',
-						),
-					),
-					'allorany' => 'all',
-				),
-				'choices' => array (
-					'left' => 'Left to Right',
-					'top' => 'Top to Bottom',
-					'radial' => 'Radial',
-				),
-				'other_choice' => 0,
-				'save_other_choice' => 0,
-				'default_value' => '',
-				'layout' => 'horizontal',
-			),
-			array (
-				'key' => 'field_51e662e675b88',
-				'label' => 'Background gradient start',
-				'name' => 'bg_gradient_start',
-				'type' => 'color_picker',
-				'instructions' => 'The starting color for the background gradient.',
-				'conditional_logic' => array (
-					'status' => 1,
-					'rules' => array (
-						array (
-							'field' => 'field_51e66262c8d40',
-							'operator' => '==',
-							'value' => 'gradient',
-						),
-					),
-					'allorany' => 'all',
-				),
-				'default_value' => '',
-			),
-			array (
-				'key' => 'field_51e6631075b89',
-				'label' => 'Background gradient stop',
-				'name' => 'bg_gradient_stop',
-				'type' => 'color_picker',
-				'instructions' => 'The stopping color for the background gradient.',
-				'conditional_logic' => array (
-					'status' => 1,
-					'rules' => array (
-						array (
-							'field' => 'field_51e66262c8d40',
-							'operator' => '==',
-							'value' => 'gradient',
-						),
-					),
-					'allorany' => 'all',
-				),
-				'default_value' => '',
-			),
 		),
 		'location' => array (
 			array (
@@ -316,7 +174,7 @@ if(function_exists("register_field_group")) {
 				array (
 					'param' => 'page_template',
 					'operator' => '!=',
-					'value' => 'page-column copy.php',
+					'value' => 'page-column.php',
 					'order_no' => 1,
 					'group_no' => 0,
 				),
