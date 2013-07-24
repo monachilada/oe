dbea9298e752c81ba934f7d53b8976d458e70ac1
Mike Pierce <mike@someoneandsons.net>
2013-07-24 17:49:42 +0200

    Fix allowing top level pages.

--- front-page.php
+++ front-page.php
@@ -4,11 +4,11 @@ Template name: Home Page
 */
 ?>
 <?php get_header(); ?>
-	<?php $pages = get_posts(array('post_type' => 'page', 'post_parent' => get_option('page_for_posts'), 'posts_per_page' => 999, 'orderby' => 'menu_order', 'order' => 'ASC')); ?>
+	<?php $pages = get_pages(array('parent' => get_option('page_on_front'), 'hierarchical' => 0, 'sort_column' => 'menu_order', 'sort_order' => 'ASC', 'exclude' => array(get_option('page_for_posts')))); ?>
 	<?php if(count($pages) > 0): ?>
 		<?php foreach($pages as $post): setup_postdata($post); ?>
 			<? $template = str_replace('.php', '', get_post_meta(get_the_ID(), '_wp_page_template', true)); ?>
-			<? if($template == 'default') $template = 'page-section'; ?>
+			<? if($template == 'default') $template = 'page'; ?>
 			<?php get_template_part('parts/background'); ?>
 			<section id="<?= $post->post_name ?>" class="<? the_field('color_scheme') ?>">
 				<?php get_template_part('parts/loop', $template); ?>
