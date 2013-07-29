c777b213b74161a0024454e456ac5b6aae6d632c
Mike Pierce <mike@someoneandsons.net>
2013-07-29 16:48:51 +0200

    Expaned markup for blog posts with additional meta data

--- parts/loop-post.php
+++ parts/loop-post.php
@@ -1,11 +1,14 @@
 <article class="post row">
-	<h5 class="col span4 meta"><time datetime="<?php the_time('Y-m-d'); ?>" pubdate><?php the_time('M'); ?><span><?php the_time('d'); ?></span><?php the_time('Y'); ?></time><?php comments_popup_link('Leave a Comment', '<span>1</span> Comment', '<span>%</span> Comments'); ?></h5>
+	<h5 class="col span4 meta"><time datetime="<?php the_time('Y-m-d'); ?>" pubdate><?php the_time('M'); ?><span><?php the_time('d'); ?></span><?php the_time('Y'); ?></time><?php comments_popup_link('<span>Leave</span> a comment', '<span>1</span> Comment', '<span>%</span> Comments'); ?></h5>
 	<div class="col span20">		
 		<?php if(has_post_thumbnail()): ?>
 			<?php the_post_thumbnail('large'); ?>
 		<?php endif; ?>
-		<h2><?php the_title(); ?></h2>		
-		<?php the_content(); ?>		
+		<h2><a href="<? the_permalink() ?>"><?php the_title(); ?></a></h2>
+		<h6><span class="spacer">Author: <? the_author_posts_link() ?></span> <span class="spacer">Category: <? the_category(', ') ?></span> <span class="spacer"><? the_tags() ?></span></h6>
+		<div class="spacer-top">
+			<?php the_content('Read more'); ?>
+		</div>
 		<?php comments_template( '', true ); ?>
 	</div>
 </article>
